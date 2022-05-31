8a67701f66b7f4f6dd493ac2edb0c4060e75606b
KaoneWatlhaga02 <102308906+KaoneWatlhaga02@users.noreply.github.com>
2022-05-31 09:40:34 +0200

    Add files via upload

---
+++ LiquidCinemas/main.js
@@ -0,0 +1,30 @@
+var swiper = new Swiper(".home", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay:{
+          delay: 5000,
+          disableOnInteraction: false,
+        },
+        pagination:{
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+
+let header= document.querySelector('header');
+let menu= document.querySelector('#menu-icon');
+let navbar= document.querySelector('.navbar');
+
+menu.onclick= () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
